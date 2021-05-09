47e2a2d482823bb2930f2a0a84fb4dfb3fada915
Darshan Satya <darshans1989@gmail.com>
2021-05-09 11:49:45 -0400

    multi file redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Counter from './components/Counter';
-
+import Auth from "./components/Auth";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <Counter />
+    <>
+      <Header />
+      <Auth />
+      <UserProfile />
+      <Counter />
+    </>
   );
 }
 
